b98b230d538768c31838be36b45c4610f31f7353
Stefano Mosconi <stefano.mosconi@gmail.com>
2020-12-21 22:13:23 +0200

    Modifying the code so that it returns the Swimmer item only once per swimmer.
    Inspiration from https://stackoverflow.com/questions/45496764/creating-scrapy-array-of-items-with-multiple-parse

--- tempusopen/spiders/records_spider.py
+++ tempusopen/spiders/records_spider.py
@@ -2,6 +2,7 @@ import scrapy
 from tempusopen.settings import swimmers
 from tempusopen.items import Swimmer, Time, Style
 from scrapy.loader import ItemLoader
+import logging
 
 
 class BaseUrl(scrapy.Item):
@@ -16,18 +17,25 @@ class RecordsSpider(scrapy.Spider):
         base_url = ('https://www.tempusopen.fi/index.php?r=swimmer/index&Swimmer[first_name]={firstname}&'
                     'Swimmer[last_name]={lastname}&Swimmer[searchChoice]=1&Swimmer[swimmer_club]={team}&'
                     'Swimmer[class]=1&Swimmer[is_active]=1')
+        # swimmers is a dictionary that contains the {'name', 'surname', 'team'}
+        # of the swimmers we want to crawl.
+        # At the moment we need to give exact names since we search for the swimmer among
+        # all the swimmers of the site and if we have multiple results
+        # we only .get() the first one
         urls = [base_url.format_map(x) for x in swimmers]
-
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        # For each swimmer we find in the search results we get his/her URL and...
         swimmer_url = response.xpath('//table//tbody/tr/td/a[@class="view"]/@href').get()
         swimmer = Swimmer()
+        # ... we fire a request to parse the records
         yield response.follow(swimmer_url, callback=self.parse_records, meta={'swimmer': swimmer})
 
     def parse_records(self, response):
-        distances = response.xpath('//table//tbody/tr/td/a[@class="view"]/@href').extract()
+        # now we are on a page that has the swimmer data, as you can see below
+        # id, name, team, year of birth, status and license number.
         swimmer_data = response.xpath("//div[@class='container main']//"
                                       "div[@class='sixteen columns']//text()").extract()
         swimmer = response.meta['swimmer']
@@ -40,13 +48,31 @@ class RecordsSpider(scrapy.Spider):
                                       "//text()").extract()
         swimmer['born'] = swimmer_data[2].strip('\n')
         swimmer['license'] = swimmer_data[4].strip('\n')
-        for url in distances:
-            yield response.follow(url, callback=self.parse_distances, meta={'swimmer': swimmer})
+        # This page also has all the different styles that the swimmer has ever swam
+        # but it only has the personal best time of the swimmer and we really would
+        # like to gather all the times for the different style
+        # so we grab all the URLs of the various styles which will lead us to a page
+        # that has all the times for that style
+        distances = response.xpath('//table//tbody/tr/td/a[@class="view"]/@href').extract()
+        # BUT because of the way how scrapy works rather than using a normal 'for' loop we need
+        # to go through each URL one by one and exhaust the urls in distances before
+        # returning a swimmer item back (if we don't do that we will return the same swimmer item
+        # multiple times rather than once per swimmer).
+        #
+        # This is achieved by popping the first url and passing the distances_urls through
+        # the next level and checking that we are really done with this swimmer before
+        # firing a 'return' (in the next level of parsing)
+        url = distances.pop()
+        yield response.follow(url, callback=self.parse_distances, meta={'swimmer': swimmer,
+                                                                        'distances': distances})
 
     def parse_distances(self, response):
+        # Finally we have gotten to the page that has all the time for the given distance
         swimmer = response.meta['swimmer']
+        # We create the Style item that will collect all the times...
         style = Style()
         try:
+            # TODO Maybe we could use a defaultdict here
             swimmer['styles']
         except:
             swimmer['styles'] = []
@@ -54,15 +80,32 @@ class RecordsSpider(scrapy.Spider):
         distance = distance.strip().split('\n')[1]
         style['name'] = distance
         try:
+            # TODO Maybe we could use a defaultdict here
             style['times']
         except:
             style['times'] = []
+        # ... and append it to the styles that the swimmer has swam
         swimmer['styles'].append(style)
+        # Then we go to the table that has all the times for this style and
+        # collect time, date and competition
         table_rows = response.xpath("//table//tbody/tr")
         for tr in table_rows:
             t = Time()
             t['time'] = tr.xpath("./td[1]/text()").extract_first().strip("\n\xa0")
             t['date'] = tr.xpath("./td[4]/text()").extract_first()
             t['competition'] = tr.xpath("./td[5]/text()").extract_first()
+            # and append this time to the list of times for the style
             style['times'].append(t)
-        return swimmer
+        distances = response.meta['distances']
+        # This is where the magic happens...
+        if distances:
+            # we go to the next style by popping the url out of the distances_url list...
+            url = distances.pop()
+            # ... and recursively calling this parse_distances method until we exhaust
+            # the distances urls list (yeah we are calling ourselves :/ )
+            yield response.follow(url, callback=self.parse_distances, meta={'swimmer': swimmer,
+                                                                            'distances': distances})
+        else:
+            # and finally if we really are done with the styles for this wimmer
+            # we return a swimmer item. Exactly once per every swimmer.
+            yield swimmer
