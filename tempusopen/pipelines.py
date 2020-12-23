25928d9a0348625214f70180f2c89cbe04aa5d12
Stefano Mosconi <stefano.mosconi@gmail.com>
2020-12-23 13:00:12 +0200

    Doing the Excel creation right from a custom Exporter

--- tempusopen/pipelines.py
+++ tempusopen/pipelines.py
@@ -7,8 +7,9 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy.exporters import JsonItemExporter
+from tempusopen.exporters import XlsxItemExporter
 
-class TempusopenPipeline:
+class JsonPipeline:
     def open_spider(self, spider):
         self.file = open('swimmers.json', 'wb')
         self.exporter = JsonItemExporter(self.file, encoding='utf-8')
@@ -17,6 +18,19 @@ class TempusopenPipeline:
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+class XlsxPipeline:
+    def open_spider(self, spider):
+        self.file = 'swimmers.xlsx'
+        self.exporter = XlsxItemExporter(self.file, encoding='utf-8')
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
 
     def process_item(self, item, spider):
         self.exporter.export_item(item)
